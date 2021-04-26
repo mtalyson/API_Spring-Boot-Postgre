25aec7edcb700161bf906642fed2e80af2484883
Talyson Moreira <talyson.penha@gmail.com>
2021-04-26 16:53:14 -0300

    Project created

---
+++ src/test/java/com/mtalyson/springpostgres/SpringpostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtalyson.springpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
